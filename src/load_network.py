6bda1246e82ee9c8db416785cca65ca87d3355c6
regstrtn <mohammadluqman@Mohammads-MacBook-Pro.local>
2017-03-09 15:56:53 +0530

    add files

--- src/load_network.py
+++ src/load_network.py
@@ -11,12 +11,12 @@ def dd(G, plotname):
 	dmax=max(degree_sequence)
 	x = range(0, dmax+1)
 	#plt.loglog(degree_sequence,'b-',marker='o')
-	#plt.loglog(degree_sequence)
-	plt.plot(degree_sequence[0:100])	
+	plt.loglog(degree_sequence)
+	#plt.plot(degree_sequence[0:100])	
 	plt.title(plotname)
 	plt.ylabel("degree")
 	plt.xlabel("rank")
-	plt.axes([0, 100, 0, 1])
+	plt.axis([0, 100, 0, 1])
 	plt.style.use('ggplot')
 	# draw graph in inset
 	
@@ -57,19 +57,19 @@ def makegraph(filename):
 	
 	return pos, neg, total
 
-inputfile = "soc-sign-epinions.txt"
+inputfile = "../data/soc-sign-epinions.txt"
 pos, neg, total = makegraph(inputfile)
 print(pos.number_of_nodes()+ neg.number_of_nodes(), pos.number_of_edges()+neg.number_of_edges())
 print(total.number_of_nodes(), total.number_of_edges())
 
-'''
+
 #Degree distribution plots
-dd(pos, "posdd")
-dd(neg, "negdd")
-dd(total, "totaldd")
-'''
-print(inputfile)
-print(nx.info(pos), nx.info(neg))
+dd(pos, "../plots/posdd")
+dd(neg, "../plots/negdd")
+dd(total, "../plots/totaldd")
+
+#print(inputfile)
+#print(nx.info(pos), nx.info(neg))
 
 '''
 print("Indegree: ")
@@ -85,8 +85,8 @@ print(pos.number_of_edges(), neg.number_of_edges())
 '''
 
 print("Degree Centrality: ")
-print(sum(nx.algorithms.degree_centrality(pos).values()), sum(nx.algorithms.degree_centrality(neg).values()))
+#print(sum(nx.algorithms.degree_centrality(pos).values()), sum(nx.algorithms.degree_centrality(neg).values()))
 
 print("Reciprocity: ")
-print("Positive: ",nx.algorithms.overall_reciprocity(pos), " Negative: ",nx.algorithms.overall_reciprocity(neg))
+#print("Positive: ",nx.algorithms.overall_reciprocity(pos), " Negative: ",nx.algorithms.overall_reciprocity(neg))
 
